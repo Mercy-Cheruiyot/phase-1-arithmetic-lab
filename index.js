fc8b8eccd759a22c76bf9652c70bb7205c60f298
Mercy <mercycheruiyot2013@gmail.com>
2022-08-20 15:42:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//equation whose multiplied results equates to 62
+let num1=31;
+let num2=2;
+let multiply=(num1*num2);
+console.log(multiply)
+//random integer  greater than 0
+let random= (Math.round(Math.random()*10)+1);
+console.log(random)
+//Calculates the modulus equal to 4
+let num3=28;
+let num4=6;
+let mod=(num3%num4);
+console.log(mod);
+//returns the maximum number which is 20
 
-// Write your code here
+let max=(Math.max(5,6,7,9,14,15,20));
+console.log(max)
